7df9ced35824e93b5a1bc1c6ee2428c24b23ecdb
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2016-09-23 11:35:40 -0300

    organizing files and things

--- main.js
+++ main.js
@@ -1,64 +1,73 @@
 'use strict';
 
-const cube = document.getElementsByClassName('cube')[0];
+(function(window, document) {
 
-const ARROW_KEYS = {
-	LEFT: 37,
-	UP: 38,
-	RIGHT: 39,
-	DOWN: 40,
-};
+	const cube = document.querySelector('.js-cube');
 
-document.addEventListener('keydown', (event) => {
-	if (event.keyCode === ARROW_KEYS.LEFT) {
-		rotateLeft();
-	} else if (event.keyCode === ARROW_KEYS.UP) {
-		rotateUp();
-	} else if (event.keyCode === ARROW_KEYS.RIGHT) {
-		rotateRight();
-	} else if (event.keyCode === ARROW_KEYS.DOWN) {
-		rotateDown();
-	}
-});
-
-const rotateLeft = () => {
-	const rotateY = getAngle(cube).rotateY;
-	let deg = !rotateY ? 0 : rotateY;
-	cube.style.transform = `rotateY(${--deg}deg)`;
-};
-
-const rotateUp = () => {
-	const rotateX = getAngle(cube).rotateX;
-	let deg = !rotateX ? 0 : rotateX;
-	cube.style.transform = `rotateX(${++deg}deg)`;
-};
-
-const rotateDown = () => {
-	const rotateX = getAngle(cube).rotateX;
-	let deg = !rotateX ? 0 : rotateX;
-	cube.style.transform = `rotateX(${--deg}deg)`;
-};
-
-const rotateRight = () => {
-	const rotateY = getAngle(cube).rotateY;
-	let deg = !rotateY ? 0 : rotateY;
-	cube.style.transform = `rotateY(${++deg}deg)`;
-};
-
-const getAngle = (element) => {
-	const style = window.getComputedStyle(element);
-	const transform = style.getPropertyValue('transform');
-
-	const values = transform.split('(')[1].split(')')[0].split(',');
-	const pi = Math.PI;
-	const sinB = parseFloat(values[8]);
-	const b = Math.round(Math.asin(sinB) * 180 / pi);
-	const cosB = Math.cos(b * pi / 180);
-	const matrixVal10 = parseFloat(values[9]);
-	const a = Math.round(Math.asin(-matrixVal10 / cosB) * 180 / pi);
-
-	return {
-		rotateX: a,
-		rotateY: b,
+	const ARROW_KEYS = {
+		LEFT: 37,
+		UP: 38,
+		RIGHT: 39,
+		DOWN: 40,
 	};
-};
+
+	const rotate = ({ element, rotateX, rotateY }) => {
+		element.style.cssText = `transform: rotateX(${rotateX}deg), rotateY(${--rotateY}deg)`;
+	}
+
+	const rotateLeft = () => {
+		const degs = getAngle(cube);
+		let rotateX = degs.rotateX ? degs.rotateX : 0;
+		let rotateY = degs.rotateY ? degs.rotateY : 0;
+		cube.style.cssText = `transform: rotateX(${rotateX}deg), rotateY(${--rotateY}deg)`;
+	};
+
+	const rotateUp = () => {
+		const rotateX = getAngle(cube).rotateX;
+		let deg = !rotateX ? 0 : rotateX;
+		cube.style.transform = `rotateX(${++deg}deg)`;
+	};
+
+	const rotateDown = () => {
+		const rotateX = getAngle(cube).rotateX;
+		let deg = !rotateX ? 0 : rotateX;
+		cube.style.transform = `rotateX(${--deg}deg)`;
+	};
+
+	const rotateRight = () => {
+		const rotateY = getAngle(cube).rotateY;
+		let deg = !rotateY ? 0 : rotateY;
+		cube.style.transform = `rotateY(${++deg}deg)`;
+	};
+
+	const getAngle = (element) => {
+		const style = window.getComputedStyle(element);
+		const transform = style.getPropertyValue('transform');
+
+		const values = transform.split('(')[1].split(')')[0].split(',');
+		const pi = Math.PI;
+		const sinB = parseFloat(values[8]);
+		const b = Math.round(Math.asin(sinB) * 180 / pi);
+		const cosB = Math.cos(b * pi / 180);
+		const matrixVal10 = parseFloat(values[9]);
+		const a = Math.round(Math.asin(-matrixVal10 / cosB) * 180 / pi);
+
+		return {
+			rotateX: a,
+			rotateY: b,
+		};
+	};
+
+	document.addEventListener('keydown', (event) => {
+		if (event.keyCode === ARROW_KEYS.LEFT) {
+			rotateLeft();
+		} else if (event.keyCode === ARROW_KEYS.UP) {
+			rotateUp();
+		} else if (event.keyCode === ARROW_KEYS.RIGHT) {
+			rotateRight();
+		} else if (event.keyCode === ARROW_KEYS.DOWN) {
+			rotateDown();
+		}
+	});
+
+})(window, document);
